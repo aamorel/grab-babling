5af22166c833856e70742174bdd4b6b66cb96867
Aurelien Morel <aurelien.morel.arthur@gmail.com>
2020-12-18 11:09:11 +0100

    added closed loop controller test

--- src/test_baxter_env.py
+++ src/test_baxter_env.py
@@ -1,25 +1,31 @@
 import gym
-
-baxter_env = gym.make('gym_baxter_grabbing:baxter_grabbing-v1', display=False)
-
-for i in range(1000):
-    o, r, eo, inf = baxter_env.step([0.2, 0, -0.1, 0.2, 0.2, -0.2, -0.2, 1])
+import controllers
+import numpy as np
+baxter_env = gym.make('gym_baxter_grabbing:baxter_grabbing-v1', display=True)
+controller_info = {'pause_frac': 0.66, 'n_iter': 600, 'ADDITIONAL_GENES': 1}
+individual = np.random.rand(344) * 2 - 1
+controller = controllers.ClosedLoopEndPauseGripAssumption(individual, controller_info)
+action = controller.initial_action
+for i in range(600):
+    o, r, eo, inf = baxter_env.step(action)
     if i % 30 == 0:
         print(o[2])
         
 
 baxter_env.reset()
 print('reset made')
-
-for i in range(1000):
-    o, r, eo, inf = baxter_env.step([0.2, 0, -0.1, 0.2, 0.2, -0.2, -0.2, 1])
+action = controller.initial_action
+for i in range(600):
+    o, r, eo, inf = baxter_env.step(action)
     if i % 30 == 0:
         print(o[2])
+        
 
 baxter_env.reset()
 print('reset made')
-
-for i in range(1000):
-    o, r, eo, inf = baxter_env.step([0.2, 0, -0.1, 0.2, 0.2, -0.2, -0.2, 1])
+action = controller.initial_action
+for i in range(600):
+    o, r, eo, inf = baxter_env.step(action)
     if i % 30 == 0:
         print(o[2])
+        
