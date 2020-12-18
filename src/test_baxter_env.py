a8c05d4ee9e3a3433c715ccba2e192fd635faa35
Aurelien Morel <aurelien.morel.arthur@gmail.com>
2020-12-18 11:44:37 +0100

    corrected test controllers

--- src/test_baxter_env.py
+++ src/test_baxter_env.py
@@ -2,12 +2,15 @@ import gym
 import controllers
 import numpy as np
 baxter_env = gym.make('gym_baxter_grabbing:baxter_grabbing-v1', display=True)
+baxter_env.set_steps_to_roll(10)
+
 controller_info = {'pause_frac': 0.66, 'n_iter': 600, 'ADDITIONAL_GENES': 1}
 individual = np.random.rand(344) * 2 - 1
 controller = controllers.ClosedLoopEndPauseGripAssumption(individual, controller_info)
 action = controller.initial_action
 for i in range(600):
     o, r, eo, inf = baxter_env.step(action)
+    action = controller.get_action(i, o)
     if i % 30 == 0:
         print(o[2])
         
@@ -17,6 +20,7 @@ print('reset made')
 action = controller.initial_action
 for i in range(600):
     o, r, eo, inf = baxter_env.step(action)
+    action = controller.get_action(i, o)
     if i % 30 == 0:
         print(o[2])
         
@@ -26,6 +30,7 @@ print('reset made')
 action = controller.initial_action
 for i in range(600):
     o, r, eo, inf = baxter_env.step(action)
+    action = controller.get_action(i, o)
     if i % 30 == 0:
         print(o[2])
         
