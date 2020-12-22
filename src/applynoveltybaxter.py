26420118d227a58f39e4b6c10ce1aa73c1b21077
Aurelien Morel <aurelien.morel.arthur@gmail.com>
2020-12-22 16:56:18 +0100

    possibly corrected reset determinism

--- src/applynoveltybaxter.py
+++ src/applynoveltybaxter.py
@@ -16,11 +16,11 @@ PLOT = True
 DISPLAY_HOF = False
 DISPLAY_RAND = False
 DISPLAY_TRIUMPHANTS = False
-EVAL_SUCCESSFULL = True
+EVAL_SUCCESSFULL = False
 
 # choose parameters
 POP_SIZE = 100
-NB_GEN = 5000
+NB_GEN = 2000
 NB_KEYPOINTS = 3
 GENE_PER_KEYPOINTS = 7
 GENES = 343
@@ -35,7 +35,7 @@ DIFF_OR_THRESH = 0.4  # threshold for clustering grasping orientations
 COV_LIMIT = 0.1  # threshold for changing behavior descriptor in change_bd ns
 N_LAG = int(200 / NB_STEPS_TO_ROLLOUT)  # number of steps before the grip time used in the multi_full_info BD
 ARCHIVE_LIMIT = 2500
-NB_CELLS = 100  # number of cells for measurement
+NB_CELLS = 1000  # number of cells for measurement
 
 
 # global variable for the environment
